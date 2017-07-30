2d974042d7a025d79d557f8cfc575fd75ab2e5a3
Jonathan <jonathan@koloa-designs.com>
2017-07-30 15:19:15 -0400

    Initial Commit

---
+++ J&S Important Memories App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  J&S Important Memories App
+//
+//  Created by Jonathan Fernandes on 7/30/17.
+//  Copyright © 2017 Jonathan Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
