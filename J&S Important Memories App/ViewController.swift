dadac3610d88cc7b7da511f9c43b829507cad1ac
Jonathan <jonathan@koloa-designs.com>
2017-08-10 21:21:55 -0400

    Abstracted logic to pull color component to separate enum. Also updated view controller to work with updated model as well as notes for code

--- J&S Important Memories App/ViewController.swift
+++ J&S Important Memories App/ViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Call all outlets to view controller first
     @IBOutlet weak var importantMemoryDateLabel: UILabel!
     @IBOutlet weak var importantMemoryDescription: UILabel!
     @IBOutlet weak var importantMemoryLabel: UILabel!
     @IBOutlet weak var importantMemoryButton: UIButton!
+    
+    // Create instances of the memories model and colors model
     let memories = MemoriesProvider()
     let colors = ColorProvider()
     
+    // Handles changing content all at once
     func setContentForLabels() {
         let currentMemory = memories.generateRandomMemory()
         importantMemoryDateLabel.text = currentMemory.date
@@ -24,16 +28,18 @@ class ViewController: UIViewController {
         importantMemoryLabel.text = currentMemory.whatIRemember
     }
     
+    // Handles changing color scheme for all parts of the app
     func setColorForLabels() {
         let currentColorWheel = colors.generateRandomColor()
         importantMemoryDateLabel.backgroundColor = currentColorWheel.headerColor
         view.backgroundColor = currentColorWheel.backgroundColor
         importantMemoryButton.backgroundColor = currentColorWheel.buttonColor
-        importantMemoryButton.layer.borderColor = currentColorWheel.borderColor.cgColor
+        importantMemoryButton.layer.borderColor = currentColorWheel.borderColor
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Intialize content and colors
         setContentForLabels()
         setColorForLabels()
     }
@@ -43,6 +49,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // When action is called, randomize the next memory and color to repopulate view
     @IBAction func showMemory() {
         setContentForLabels()
         setColorForLabels()
--- J&S Important Memories App/colorProvider.swift
+++ J&S Important Memories App/colorProvider.swift
@@ -9,26 +9,98 @@
 import UIKit
 import GameKit
 
+// Enum to model a instance of a color that can be converted to UIColor or CGColor as needed
+enum ColorComponent {
+    case rgb(red: Float, green: Float, blue: Float, alpha: Float)
+    case hsb(hue: CGFloat, saturation: CGFloat, brightness: CGFloat, alpha: CGFloat)
+    
+    func toUIColor() -> UIColor {
+        switch self {
+        case .rgb(let red, let green, let blue, let alpha):
+            return UIColor(colorLiteralRed: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
+        case .hsb(let hue, let saturation, let brightness, let alpha):
+            return UIColor(hue: hue/360.0, saturation: saturation/100.0, brightness: brightness/100.0, alpha: alpha)
+        }
+    }
+    
+    func toCGColor() -> CGColor {
+        switch self {
+        case .rgb(let red, let green, let blue, let alpha):
+            return UIColor(colorLiteralRed: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha).cgColor
+        case .hsb(let hue, let saturation, let brightness, let alpha):
+            return UIColor(hue: hue/360.0, saturation: saturation/100.0, brightness: brightness/100.0, alpha: alpha).cgColor
+        }
+    }
+}
+
+// Since we are changing four colors at once, a struct to model a "package" of colors
 struct ColorPackage {
     let headerColor: UIColor
     let backgroundColor: UIColor
     let buttonColor: UIColor
-    let borderColor: UIColor
+    let borderColor: CGColor
     
-    init(headerR hr: Float, headerG hg: Float, headerB hb: Float, backgroundR br: Float, backgroundG bg: Float, backgroundB bb: Float, buttonR bur: Float, buttonG bug: Float, buttonB bub: Float, borderR bor: Float, borderG bog: Float, borderB bob: Float) {
-        self.headerColor = UIColor(red: CGFloat(hr/255.0), green: CGFloat(hg/255.0), blue: CGFloat(hb/255.0), alpha: 1.0)
-        self.backgroundColor = UIColor(red: CGFloat(br/255.0), green: CGFloat(bg/255.0), blue: CGFloat(bb/255.0), alpha: 1.0)
-        self.buttonColor = UIColor(red: CGFloat(bur/255.0), green: CGFloat(bug/255.0), blue: CGFloat(bub/255.0), alpha: 1.0)
-        self.borderColor = UIColor(red: CGFloat(bor/255.0), green: CGFloat(bog/255.0), blue: CGFloat(bob/255.0), alpha: 1.0)
+    // each color initialized with ColorComponent enum we created
+    init(headerColor: ColorComponent, backgroundColor: ColorComponent, buttonColor: ColorComponent, borderColor: ColorComponent) {
+        self.headerColor = headerColor.toUIColor()
+        self.backgroundColor = backgroundColor.toUIColor()
+        self.buttonColor = buttonColor.toUIColor()
+        self.borderColor = borderColor.toCGColor()
     }
 }
 
+// The following struct will be the instance we call in our controller to handle the randomizing of colors
 struct ColorProvider {
     let colors: [ColorPackage] = [
-        ColorPackage(headerR: 2, headerG: 136, headerB: 209, backgroundR: 3, backgroundG: 169, backgroundB: 244, buttonR: 255, buttonG: 82, buttonB: 82, borderR: 217, borderG: 44, borderB: 44),
-        ColorPackage(headerR: 56, headerG: 142, headerB: 60, backgroundR: 76, backgroundG: 175, backgroundB: 80, buttonR: 255, buttonG: 193, buttonB: 7, borderR: 217, borderG: 155, borderB: 0),
-        ColorPackage(headerR: 81, headerG: 45, headerB: 168, backgroundR: 103, backgroundG: 58, backgroundB: 183, buttonR: 158, buttonG: 158, buttonB: 158, borderR: 120, borderG: 120, borderB: 120),
-        ColorPackage(headerR: 0, headerG: 121, headerB: 107, backgroundR: 0, backgroundG: 150, backgroundB: 136, buttonR: 121, buttonG: 85, buttonB: 72, borderR: 83, borderG: 47, borderB: 34)
+        // Analogous Green with Green/Blue support and accent
+        ColorPackage(
+            headerColor: .rgb(red: 56.0, green: 142.0, blue: 60.0, alpha: 1.0),
+            backgroundColor: .rgb(red: 76.0, green: 175.0, blue: 80.0, alpha: 1.0),
+            buttonColor: .rgb(red: 68.0, green: 138.0, blue: 255.0, alpha: 1.0),
+            borderColor: .rgb(red: 17.0, green: 87.0, blue: 204.0, alpha: 1.0)
+        ),
+        // Analogous Blue with Blue/Purple support and accent
+        ColorPackage(
+            headerColor: .rgb(red: 25.0, green: 118.0, blue: 210.0, alpha: 1.0),
+            backgroundColor: .rgb(red: 33.0, green: 150.0, blue: 243.0, alpha: 1.0),
+            buttonColor: .rgb(red: 124.0, green: 77.0, blue: 255.0, alpha: 1.0),
+            borderColor: .rgb(red: 73.0, green: 26.0, blue: 204.0, alpha: 1.0)
+        ),
+        // Analogous Orange with Orange/Yellow support and accent
+        ColorPackage(
+            headerColor: .rgb(red: 245.0, green: 124.0, blue: 0.0, alpha: 1.0),
+            backgroundColor: .rgb(red: 255.0, green: 152.0, blue: 0.0, alpha: 1.0),
+            buttonColor: .rgb(red: 255.0, green: 235.0, blue: 59.0, alpha: 1.0),
+            borderColor: .rgb(red: 204.0, green: 184.0, blue: 8.0, alpha: 1.0)
+        ),
+        // Primary Red and Green compliment
+        ColorPackage(
+            headerColor: .rgb(red: 211.0, green: 47.0, blue: 47.0, alpha: 1.0),
+            backgroundColor: .rgb(red: 244.0, green: 67.0, blue: 54.0, alpha: 1.0),
+            buttonColor: .rgb(red: 76.0, green: 175.0, blue: 80.0, alpha: 1.0),
+            borderColor: .rgb(red: 25.0, green: 124.0, blue: 29.0, alpha: 1.0)
+        ),
+        // Blue and Amber compliment
+        ColorPackage(
+            headerColor: .rgb(red: 48.0, green: 63.0, blue: 159.0, alpha: 1.0),
+            backgroundColor: .rgb(red: 63.0, green: 81.0, blue: 181.0, alpha: 1.0),
+            buttonColor: .rgb(red: 255.0, green: 193.0, blue: 7.0, alpha: 1.0),
+            borderColor: .rgb(red: 204.0, green: 142.0, blue: 0.0, alpha: 1.0)
+        ),
+        // Pink and Light Green compliment
+        ColorPackage(
+            headerColor: ColorComponent.rgb(red: 194.0, green: 24.0, blue: 91.0, alpha: 1.0),
+            backgroundColor: ColorComponent.rgb(red: 223.0, green: 30.0, blue: 99.0, alpha: 1.0),
+            buttonColor: ColorComponent.rgb(red: 139.0, green: 195.0, blue: 74.0, alpha: 1.0),
+            borderColor: ColorComponent.rgb(red: 88.0, green: 144.0, blue: 23.0, alpha: 1.0)
+        ),
+        // Orange and Light Blue compliment
+        ColorPackage(
+            headerColor: ColorComponent.rgb(red: 245.0, green: 124.0, blue: 0.0, alpha: 1.0),
+            backgroundColor: ColorComponent.rgb(red: 255.0, green: 152.0, blue: 0.0, alpha: 1.0),
+            buttonColor: ColorComponent.rgb(red: 3.0, green: 169.0, blue: 244.0, alpha: 1.0),
+            borderColor: ColorComponent.rgb(red: 0.0, green: 118.0, blue: 193.0, alpha: 1.0)
+        )
     ]
     
     func generateRandomColor() -> ColorPackage {
